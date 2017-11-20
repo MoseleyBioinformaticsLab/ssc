a4db87014da9494d9887a2b2f05da2cda9c21bbf
Andrey Smelter <andrey.smelter@gmail.com>
2017-11-20 17:12:44 -0500

    Updated main file cli.

--- ssc/__main__.py
+++ ssc/__main__.py
@@ -7,68 +7,51 @@ ssc (Spin System Creator) command-line interface
 Usage:
     ssc -h | --help
     ssc --version
-    ssc group (--plpath=<path>) (--plformat=<format>) (--stype=<type>) (--dims=<labels>) (--rdims=<labels>) [--results=<path>] [--crspath=<path>]
-    ssc group (--descrfile=<path>) [--results=<path>] [--crspath=<path>]
-    ssc group (--stdin) [--results=<path>] [--crspath=<path>]
+    ssc group (--plpath=<path>) (--plformat=<format>) (--stype=<type>) (--dims=<labels>) (--rdims=<labels>) [--result=<path>] [--crs=<path>] [--view]
+    ssc visualize <grouping_result> <x_idx> <y_idx> <x_label> <y_label> <plot_title>
 
 Options:
     -h, --help                   Show this screen.
     --version                    Show version.
-    --verbose                    Print what files are processing.
+    --view                       Print results of the grouping.
     --plpath=<path>              Path to peak list.
     --plformat=<format>          Peak list format.
     --stype=<type>               Spectrum type.
     --dims=<labels>              Comma-separated dimension labels.
     --rdims=<labels>             Comma-separated root dimension labels.
-    --results=<path>             Path where results will be saved.
-    --crspath=<path>             Registration algorithm executable path [default: ssc/bin/calculate_registration]
-    --descrfile=<path>           Get arguments from description file.
-    --stdin                      Get arguments from standard input.
+    --crs=<path>                 Registration algorithm executable path [default: ssc/bin/CRS_EXE]
+    --result=<path>              Path to directory where results will be saved.
 """
 
-import sys
-import json
-import docopt
-import ssc
+from . import docopt
+from . import ssc
+from . import __version__
+from . import visualize
 
 
 def main(cmdargs):
-
     if cmdargs["group"]:
-        if cmdargs["--descrfile"] or cmdargs["--stdin"]:
-            if cmdargs["--descrfile"]:
-                with open(cmdargs["--descrfile"], "r") as infile:
-                    args = json.load(infile)
-            else:
-                args = json.loads(sys.stdin.read())
+        dims = cmdargs["--dims"].split(",") if cmdargs["--dims"] else []
+        rdims = cmdargs["--rdims"].split(",") if cmdargs["--rdims"] else []
+        view = cmdargs["--view"]
 
-            sscreator = ssc.SpinSystemCreator(peaklistpath=args["PeakListPath"],
-                                              plformat=args["PeakListFormat"],
-                                              spectrumtype=args["SpectrumType"],
-                                              dimlabels=args["DimensionLabels"],
-                                              rootdims=args["RootDimensions"],
-                                              regalgpath=cmdargs["--crspath"],
-                                              outputdirpath=cmdargs["--results"])
-        else:
-            dims = cmdargs["--dims"].split(",") if cmdargs["--dims"] else []
-            rdims = cmdargs["--rdims"].split(",") if cmdargs["--rdims"] else []
+        sscreator = ssc.SpinSystemCreator(peaklist_path=cmdargs["--plpath"],
+                                          plformat=cmdargs["--plformat"],
+                                          spectrum_type=cmdargs["--stype"],
+                                          labels=dims,
+                                          root_dims=rdims,
+                                          regalg_path=cmdargs["--crs"],
+                                          grouping_result_path=cmdargs["--result"])
+        sscreator.group(view=view)
 
-            sscreator = ssc.SpinSystemCreator(peaklistpath=cmdargs["--plpath"],
-                                              plformat=cmdargs["--plformat"],
-                                              spectrumtype=cmdargs["--stype"],
-                                              dimlabels=dims,
-                                              rootdims=rdims,
-                                              regalgpath=cmdargs["--crspath"],
-                                              outputdirpath=cmdargs["--results"])
-        sscreator.group()
+    if cmdargs["visualize"]:
 
-args = docopt.docopt(__doc__)
+        visualize.visualize_clusters(clusters_path=cmdargs["<grouping_result>"],
+                                     x_idx=int(cmdargs["<x_idx>"]),
+                                     y_idx=int(cmdargs["<y_idx>"]),
+                                     x_label=cmdargs["<x_label>"],
+                                     y_label=cmdargs["<y_label>"],
+                                     title=cmdargs["<plot_title>"])
+
+args = docopt.docopt(__doc__, version=__version__)
 main(args)
-
-
-
-# python3 ssc group --plpath=datasets/jr19_hncocacb.pks --plformat=autoassign --stype=HNcoCACB --dims=HN,N,CA/CB-1 --rdims=HN,N
-
-# python3 ssc.pyz group --plpath=datasets/jr19_hncocacb.pks --plformat=autoassign --stype=HNcoCACB --dims=HN,N,CA/CB-1 --rdims=HN,N
-# python3 ssc.pyz group --descrfile=ProblemDescriptionHNcoCACB_jr19.json
-# more ProblemDescriptionHNcoCACB_jr19.json | python3 ssc.pyz group --stdin
