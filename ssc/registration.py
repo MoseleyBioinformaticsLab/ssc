c59b84428d9e5f59b80fc56c0fd5869a46290e4a
Andrey Smelter <andrey.smelter@gmail.com>
2017-11-20 17:29:07 -0500

    Updated registration module.

--- ssc/registration.py
+++ ssc/registration.py
@@ -5,47 +5,35 @@
 ssc.registration
 ~~~~~~~~~~~~~~~~~~
 
-This module implements helper function that runs registration algorithm.
+This module implements helper function that runs the 
+registration algorithm.
 
 """
 
 import subprocess
-import sys
-import re
 import json
 
 
-def run_registration(execpath, inputlistpath, rootlistpath, regresultpath, *args):
+def run_registration(regalg_executable, input_peaklist, root_peaklist, *args):
     """Run registration algorithm executable and save results.
 
-    :param str execpath: Path to registration algorithm executable.
-    :param str inputlistpath: Path to input peak list.
-    :param str rootlistpath: Path to root peak list.
-    :param str regresultpath: Path where save registration results.
-    :param args: Extra command-line arguments necessary to run registration executable.
-    :return: Results dict.
-    :rtype: dict
+    :param str regalg_executable: Path to the registration algorithm executable.
+    :param str input_peaklist: Path to input peak list.
+    :param str root_peaklist: Path to root peak list.
+    :param args: Command-line arguments necessary to run registration executable.
+    :return: Dictionary containing registration results.
+    :rtype: :py:class:`dict`
     """
     results = {}
-    numberpattern = re.compile("[+-]?\d+(\.\d+)?")
-    cmdarguments = [execpath, inputlistpath, rootlistpath] + list(args)
-    output = subprocess.check_output(cmdarguments)
-    lines = output.decode("utf-8").splitlines()
+    command = "{} {} {} ".format(regalg_executable, input_peaklist, root_peaklist) + " ".join(list(args))
+    output = subprocess.check_output(command, shell=True)
+    lines = output.decode("utf-8")
 
     if not lines:
         return results
     else:
-        with open(regresultpath, "r") as infile:
-            results = json.load(infile)
-
+        try:
+            results = json.loads(lines)
+        except ValueError:
+            return results
     return results
-
-if __name__ == "__main__":
-
-    script = sys.argv.pop(0)
-    ipl = sys.argv.pop(0)
-    rpl = sys.argv.pop(0)
-    resultspath = sys.argv.pop(0)
-
-    r = run_registration('./bin/calculate_registration', ipl, rpl, resultspath, '-autoassign', '-noi', '-save',
-                         resultspath, '-dim', 'HN', 'N', ':', 'HN', 'N')
